8c2756e71e69f844dc1240bcda0e8d2863b35bd0
kimsatoo <diana.r.silva.d@gmail.com>
2024-11-17 10:59:11 -0300

    pesquisa binaria com lista de strings

--- capitulo1/main.py
+++ capitulo1/main.py
@@ -1,7 +1,12 @@
 import pesquisa_binaria as pb
 
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# l = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99]
 
-item = pb.pesqisa_binaria(l, 3)
-
-print(item)
+# para usar a PB em listas de strings, precisamos ordenar a lista
+l = ["maria", "jonh", "ana", "pedro", "jose", "joao", "lucas", "jessica", "julia", "julio"]
+# print(l)
+l.sort()
+# print(l)
+item = pb.pesqisa_binaria(l, "jessica")
+if item is not None:
+    print("O item", l[item], "esta na posicao", item)
