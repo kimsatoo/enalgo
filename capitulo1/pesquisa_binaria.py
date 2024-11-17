a2fb746bfe89b027c91ba66d5384a19013bc2c5b
kimsatoo <diana.r.silva.d@gmail.com>
2024-11-17 10:58:54 -0300

    add comentários para maior entendimento

--- capitulo1/pesquisa_binaria.py
+++ capitulo1/pesquisa_binaria.py
@@ -1,13 +1,15 @@
+""" pega um array ordenado e um item. se o item estiver na lista, retorna a posicao dele, se não, retorna None. dessa forma vc é capaz de saber em qual ponto da lista continuar procurando 
+"""
 
 def pesqisa_binaria(lista, item):
     # mantem o controle de que parte da lista vc esta procurando
-    baixo = 0
-    alto =len(lista) - 1 
+    baixo = 0 # num a partir da posição inicial para o meio
+    alto =len(lista) - 1 # num de elementos - o que quero achar
     
     # enquanto vc não reduziu a um elemento
     while baixo <= alto:
         # cheque o elemento do meio
-        meio = (baixo + alto) // 2
+        meio = (baixo + alto) // 2 # sera arredondado para baixo se não for par
         chute = lista[meio]
         
         # achou o item
@@ -22,4 +24,3 @@ def pesqisa_binaria(lista, item):
     
     # item não existe
     return None
-    
